521bc9b70b1aca175bc52c26806d8015ec439701
shubham2428 <34512195+shubham2428@users.noreply.github.com>
2019-06-13 02:55:30 -0700

    Add files via upload

---
+++ ReadMe.txt
@@ -0,0 +1,10 @@
+Setup
+
+1.Run training_model to train model
+2.Test prediction by running testing_model
+
+
+
+Note::This project is made on google colab so I used drive to save my model 
+.Please change path according to you
+
---
+++ testing_model.py
@@ -0,0 +1,86 @@
+import tflearn
+from PIL import Image, ImageFilter
+
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+import tflearn.datasets.mnist as mnist
+import numpy as np
+
+
+X, Y, test_x, test_y = mnist.load_data(one_hot=True)
+
+X = X.reshape([-1, 28, 28, 1])
+test_x = test_x.reshape([-1, 28, 28, 1])
+
+convnet = input_data(shape=[None, 28, 28, 1], name='input')
+
+convnet = conv_2d(convnet, 32, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = conv_2d(convnet, 64, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = fully_connected(convnet, 1024, activation='relu')
+convnet = dropout(convnet, 0.8)
+
+convnet = fully_connected(convnet, 10, activation='softmax')
+convnet = regression(convnet, optimizer='adam', learning_rate=0.01, loss='categorical_crossentropy', name='targets')
+
+model = tflearn.DNN(convnet)
+'''model.load('quicktest.model')
+'''
+
+model.load('quicktest.model')
+
+
+def imageprepare(argv):
+    """
+    This function returns the pixel values.
+    The imput is a png file location.
+    """
+    im = Image.open(argv).convert('L')
+    width = float(im.size[0])
+    height = float(im.size[1])
+    newImage = Image.new('L', (28, 28), (255))  # creates white canvas of 28x28 pixels
+
+    if width > height:  # check which dimension is bigger
+        # Width is bigger. Width becomes 20 pixels.
+        nheight = int(round((28.0 / width * height), 0))  # resize height according to ratio width
+        if (nheight == 0):  # rare case but minimum is 1 pixel
+            nheight = 1
+            # resize and sharpen
+        img = im.resize((28, nheight), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
+        wtop = int(round(((28 - nheight) / 2), 0))  # calculate horizontal position
+        newImage.paste(img, (0, 0))  # paste resized image on white canvas
+    else:
+        # Height is bigger. Heigth becomes 20 pixels.
+        nwidth = int(round((28.0 / height * width), 0))  # resize width according to ratio height
+        if (nwidth == 0):  # rare case but minimum is 1 pixel
+            nwidth = 1
+            # resize and sharpen
+        img = im.resize((nwidth, 28), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
+        wleft = int(round(((28 - nwidth) / 2), 0))  # caculate vertical pozition
+        newImage.paste(img, (0, 0))  # paste resized image on white canvas
+
+    # newImage.save("sample.png
+
+    tv = list(newImage.getdata())  # get pixel values
+
+    # normalize pixels to 0 and 1. 0 is pure white, 1 is pure black.
+    tva = [(255 - x) * 1.0 / 255.0 for x in tv]
+    print(tva)
+    '''for n,i in enumerate(tva):
+        if i > 0.9:
+            tva[n]=1.0'''
+    return tva
+
+  
+data=imageprepare("img_15.png")
+'''
+print( np.round(model.predict([test_x[2]])[0]) )
+print(test_y[2])'''
+data2=np.array(data)
+data2=data2.reshape(-1)
+data2=data2.reshape([-1, 28, 28, 1])
+print( np.round(model.predict([test_x[2]])[0]) )
